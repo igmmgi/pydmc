2d446c52e6fa0f350f5a2e48dc69450d94d01373
ian <ian.mackenzie@uni-tuebingen.de>
2021-07-20 09:37:50 +0200

    plot updates

--- pydmc/dmcplot.py
+++ pydmc/dmcplot.py
@@ -5,20 +5,22 @@ from fastkde import fastKDE
 
 
 class DmcPlot:
-    def __init__(self, dat, fig_type="summary1"):
+    def __init__(self, dat):
         self.dat = dat
-        self.fig_type = fig_type
 
     def plot(self, **kwargs):
-        """Plot summary"""
+        """Plot summary."""
+        kwargs.setdefault("fig_type", "summary1")
+        kwargs.setdefault("hspace", 0.5)
+        kwargs.setdefault("wspace", 0.5)
         if hasattr(self.dat, "prms"):
-            if self.fig_type == "summary1" and not self.dat.full_data:
-                self.fig_type = "summary2"
-            if self.fig_type == "summary1":
+            if kwargs["fig_type"] == "summary1" and not self.dat.full_data:
+                kwargs["fig_type"] = "summary2"
+            if kwargs["fig_type"] == "summary1":
                 self._plot_summary1_res_th(**kwargs)
-            elif self.fig_type == "summary2":
+            elif kwargs["fig_type"] == "summary2":
                 self._plot_summary2_res_th(**kwargs)
-            elif self.fig_type == "summary3":
+            elif kwargs["fig_type"] == "summary3":
                 self._plot_summary3_res_th(**kwargs)
         else:
             self._plot_summary1_res_ob(**kwargs)
@@ -49,9 +51,7 @@ class DmcPlot:
         plt.subplot2grid((6, 4), (4, 2), rowspan=2, colspan=2)
         self.plot_delta(show=False, **kwargs)
 
-        plt.subplots_adjust(
-            hspace=kwargs.get("hspace", 0.5), wspace=kwargs.get("wspace", 0.5)
-        )
+        plt.subplots_adjust(hspace=kwargs["hspace"], wspace=kwargs["wspace"])
         plt.show(block=False)
 
     def _plot_summary2_res_th(self, **kwargs):
@@ -72,9 +72,7 @@ class DmcPlot:
         plt.subplot2grid((3, 2), (2, 0), colspan=2)
         self.plot_delta(show=False, **kwargs)
 
-        plt.subplots_adjust(
-            hspace=kwargs.get("hspace", 0.5), wspace=kwargs.get("wspace", 0.5)
-        )
+        plt.subplots_adjust(hspace=kwargs["hspace"], wspace=kwargs["wspace"])
         plt.show(block=False)
 
     def _plot_summary3_res_th(self, **kwargs):
@@ -91,9 +89,7 @@ class DmcPlot:
         plt.subplot2grid((3, 1), (2, 0))
         self.plot_rt_error(show=False, **kwargs)
 
-        plt.subplots_adjust(
-            hspace=kwargs.get("hspace", 0.5), wspace=kwargs.get("wspace", 0.5)
-        )
+        plt.subplots_adjust(hspace=kwargs["hspace"], wspace=kwargs["wspace"])
         plt.show(block=False)
 
     def _plot_summary1_res_ob(self, **kwargs):
@@ -123,34 +119,27 @@ class DmcPlot:
         plt.subplot2grid((3, 2), (2, 1))
         self.plot_delta(show=False, **kwargs)
 
-        plt.subplots_adjust(
-            hspace=kwargs.get("hspace", 0.5), wspace=kwargs.get("wspace", 0.5)
-        )
+        plt.subplots_adjust(hspace=kwargs["hspace"], wspace=kwargs["wspace"])
         plt.show(block=False)
 
     def plot_activation(
         self,
         show=True,
-        xlim=None,
-        ylim=None,
-        xlabel="Time (ms)",
-        ylabel="E[X(t)]",
         cond_labels=("Compatible", "Incompatible"),
         legend_position="best",
         colors=("green", "red"),
         **kwargs
     ):
-        """Plot activation"""
+        """Plot activation."""
 
         if not self.dat.xt:
             print("Plotting activation function requires full_data=True")
             return
 
-        plt.plot(self.dat.eq4, "k-")
-        plt.plot(self.dat.eq4 * -1, "k--")
-
         l_kws = _filter_dict(kwargs, plt.Line2D)
 
+        plt.plot(self.dat.eq4, "k-")
+        plt.plot(self.dat.eq4 * -1, "k--")
         plt.plot(self.dat.xt[0], color=colors[0], label=cond_labels[0], **l_kws)
         plt.plot(self.dat.xt[1], color=colors[1], label=cond_labels[1], **l_kws)
         plt.plot(
@@ -158,12 +147,13 @@ class DmcPlot:
             color="black",
             **l_kws,
         )
-
-        kwargs["xlabel"] = xlabel
-        kwargs["ylabel"] = ylabel
-        kwargs["xlim"] = xlim or [0, self.dat.prms.t_max]
-        kwargs["ylim"] = ylim or [-self.dat.prms.bnds - 20, self.dat.prms.bnds + 20]
         self._plot_bounds()
+
+        kwargs.setdefault("xlim", [0, self.dat.prms.t_max])
+        kwargs.setdefault("ylim", [-self.dat.prms.bnds - 20, self.dat.prms.bnds + 20])
+        kwargs.setdefault("xlabel", "Time (ms)")
+        kwargs.setdefault("ylabel", "E[X(t)]")
+
         _adjust_plt(**kwargs)
 
         if legend_position is not None:
@@ -175,29 +165,12 @@ class DmcPlot:
     def plot_trials(
         self,
         show=True,
-        xlim=None,
-        ylim=None,
-        xlabel="Time (ms)",
-        ylabel="X(t)",
         cond_labels=("Compatible", "Incompatible"),
         legend_position="upper right",
         colors=("green", "red"),
         **kwargs
     ):
-        """Plot individual trials.
-
-        Parameters
-        ----------
-        show
-        xlim
-        ylim
-        xlabel
-        ylabel
-        cond_labels
-        legend_position
-        colors
-        kwargs
-        """
+        """Plot individual trials."""
 
         if not self.dat.xt:
             print("Plotting individual trials function requires full_data=True")
@@ -209,29 +182,24 @@ class DmcPlot:
                 labels = cond_labels
             else:
                 labels = [None, None]
-            idx = np.where(
-                np.abs(self.dat.dat_trials[0][trl, :]) >= self.dat.prms.bnds
-            )[0][0]
-            plt.plot(
-                self.dat.dat_trials[0][trl][0:idx],
-                color=colors[0],
-                label=labels[0],
-                **l_kws,
-            )
-            idx = np.where(
-                np.abs(self.dat.dat_trials[1][trl, :]) >= self.dat.prms.bnds
-            )[0][0]
-            plt.plot(
-                self.dat.dat_trials[1][trl][0:idx],
-                color=colors[1],
-                label=labels[1],
-                **l_kws,
-            )
-
-        xlim = xlim or [0, self.dat.prms.t_max]
-        ylim = ylim or [-self.dat.prms.bnds - 20, self.dat.prms.bnds + 20]
+            for comp in (0, 1):
+                idx = np.where(
+                    np.abs(self.dat.dat_trials[comp][trl, :]) >= self.dat.prms.bnds
+                )[0][0]
+                plt.plot(
+                    self.dat.dat_trials[comp][trl][0:idx],
+                    color=colors[comp],
+                    label=labels[comp],
+                    **l_kws,
+                )
         self._plot_bounds()
-        _adjust_plt(xlim=xlim, ylim=ylim, xlabel=xlabel, ylabel=ylabel, **kwargs)
+
+        kwargs.setdefault("xlim", [0, self.dat.prms.t_max])
+        kwargs.setdefault("ylim", [-self.dat.prms.bnds - 20, self.dat.prms.bnds + 20])
+        kwargs.setdefault("xlabel", "Time (ms)")
+        kwargs.setdefault("ylabel", "X(t)")
+
+        _adjust_plt(**kwargs)
 
         if legend_position:
             plt.legend(loc=legend_position)
@@ -246,40 +214,24 @@ class DmcPlot:
     def plot_pdf(
         self,
         show=True,
-        xlim=None,
-        ylim=None,
-        xlabel="Time (ms)",
-        ylabel="PDF",
         cond_labels=("Compatible", "Incompatible"),
         legend_position="upper right",
         colors=("green", "red"),
         **kwargs
     ):
-        """Plot PDF.
-
-        Parameters
-        ----------
-        show
-        xlim
-        ylim
-        xlabel
-        ylabel
-        cond_labels
-        legend_position
-        colors
-        kwargs
-        """
-
-        comp_pdf, axes1 = fastKDE.pdf(self.dat.dat[0][0])
-        incomp_pdf, axes2 = fastKDE.pdf(self.dat.dat[1][0])
+        """Plot PDF."""
 
         l_kws = _filter_dict(kwargs, plt.Line2D)
-        plt.plot(axes1, comp_pdf, color=colors[0], label=cond_labels[0], **l_kws)
-        plt.plot(axes2, incomp_pdf, color=colors[1], label=cond_labels[1], **l_kws)
+        for comp in (0, 1):
+            pdf, axes = fastKDE.pdf(self.dat.dat[comp][0])
+            plt.plot(axes, pdf, color=colors[comp], label=cond_labels[comp], **l_kws)
 
-        xlim = xlim or [0, self.dat.prms.t_max]
-        ylim = ylim or [0, 0.01]
-        _adjust_plt(xlim=xlim, ylim=ylim, xlabel=xlabel, ylabel=ylabel, **kwargs)
+        kwargs.setdefault("xlim", [0, self.dat.prms.t_max])
+        kwargs.setdefault("ylim", [0, 0.01])
+        kwargs.setdefault("xlabel", "Time (ms)")
+        kwargs.setdefault("ylabel", "PDF")
+
+        _adjust_plt(**kwargs)
 
         if legend_position:
             plt.legend(loc=legend_position)
@@ -290,29 +242,12 @@ class DmcPlot:
     def plot_cdf(
         self,
         show=True,
-        xlim=None,
-        ylim=(0, 1.0),
-        xlabel="Time (ms)",
-        ylabel="CDF",
         cond_labels=("Compatible", "Incompatible"),
         legend_position="lower right",
         colors=("green", "red"),
         **kwargs
     ):
-        """Plot CDF.
-
-        Parameters
-        ----------
-        show
-        xlim
-        ylim
-        xlabel
-        ylabel
-        cond_labels
-        legend_position
-        colors
-        kwargs
-        """
+        """Plot CDF."""
 
         l_kws = _filter_dict(kwargs, plt.Line2D)
         if hasattr(self.dat, "prms"):
@@ -326,8 +261,7 @@ class DmcPlot:
                     **l_kws,
                 )
 
-            xlim = xlim or [0, self.dat.prms.t_max]
-            ylim = ylim or [0, 1.05]
+            kwargs.setdefault("xlim", [0, self.dat.prms.t_max])
 
         if not hasattr(self.dat, "prms"):
 
@@ -343,13 +277,19 @@ class DmcPlot:
                     **l_kws,
                 )
 
-            xlim = xlim or [
-                np.min(self.dat.mean_bin) - 100,
-                np.max(self.dat.delta.mean_bin) + 100,
-            ]
-            ylim = ylim or [0, 1.05]
+            kwargs.setdefault(
+                "xlim",
+                [
+                    np.min(self.dat.mean_bin) - 100,
+                    np.max(self.dat.delta.mean_bin) + 100,
+                ],
+            )
 
-        _adjust_plt(xlim=xlim, ylim=ylim, xlabel=xlabel, ylabel=ylabel, **kwargs)
+        kwargs.setdefault("ylim", [0, 1.05])
+        kwargs.setdefault("xlabel", "Time (ms)")
+        kwargs.setdefault("ylabel", "CDF")
+
+        _adjust_plt(**kwargs)
 
         if legend_position:
             plt.legend(loc=legend_position)
@@ -360,27 +300,12 @@ class DmcPlot:
     def plot_caf(
         self,
         show=True,
-        ylim=(0, 1.1),
-        xlabel="RT Bin",
-        ylabel="CAF",
         cond_labels=("Compatible", "Incompatible"),
         legend_position="lower right",
         colors=("green", "red"),
         **kwargs
     ):
-        """Plot CAF.
-
-        Parameters
-        ----------
-        show
-        ylim
-        xlabel
-        ylabel
-        cond_labels
-        legend_position
-        colors
-        kwargs
-        """
+        """Plot CAF."""
 
         kwargs.setdefault("marker", "o")
         kwargs.setdefault("markersize", 4)
@@ -398,7 +323,12 @@ class DmcPlot:
         plt.xticks(
             range(1, self.dat.n_caf + 1), [str(x) for x in range(1, self.dat.n_caf + 1)]
         )
-        _adjust_plt(ylim=ylim, xlabel=xlabel, ylabel=ylabel, **kwargs)
+
+        kwargs.setdefault("ylim", [0, 1.1])
+        kwargs.setdefault("xlabel", "RT Bin")
+        kwargs.setdefault("ylabel", "CAF")
+
+        _adjust_plt(**kwargs)
 
         if legend_position:
             plt.legend(loc=legend_position)
@@ -406,26 +336,8 @@ class DmcPlot:
         if show:
             plt.show(block=False)
 
-    def plot_delta(
-        self,
-        show=True,
-        xlim=None,
-        ylim=None,
-        xlabel="Time (ms)",
-        ylabel=r"$\Delta$",
-        **kwargs
-    ):
-        """Plot reaction-time delta plots.
-
-        Parameters
-        ----------
-        show
-        xlim
-        ylim
-        xlabel
-        ylabel
-        kwargs
-        """
+    def plot_delta(self, show=True, **kwargs):
+        """Plot reaction-time delta plots."""
 
         kwargs.setdefault("color", "black")
         kwargs.setdefault("marker", "o")
@@ -433,43 +345,29 @@ class DmcPlot:
 
         datx, daty = self.dat.delta["mean_bin"], self.dat.delta["mean_effect"]
 
-        xlim = xlim or [np.min(datx) - 100, np.max(datx) + 100]
-        ylim = ylim or [np.min(daty) - 25, np.max(daty) + 25]
-
         l_kws = _filter_dict(kwargs, plt.Line2D)
         plt.plot(datx, daty, **l_kws)
-        _adjust_plt(xlim=xlim, ylim=ylim, xlabel=xlabel, ylabel=ylabel, **kwargs)
+
+        kwargs.setdefault("xlim", [np.min(datx) - 100, np.max(datx) + 100])
+        kwargs.setdefault("ylim", [np.min(daty) - 25, np.max(daty) + 25])
+        kwargs.setdefault("xlabel", "Time (ms)")
+        kwargs.setdefault("ylabel", r"$\Delta$")
+
+        _adjust_plt(**kwargs)
 
         if show:
             plt.show(block=False)
 
     def plot_rt_correct(
-        self,
-        show=True,
-        ylim=None,
-        xlabel=None,
-        ylabel="RT Correct [ms]",
-        cond_labels=("Compatible", "Incompatible"),
-        **kwargs
+        self, show=True, cond_labels=("Compatible", "Incompatible"), **kwargs
     ):
-        """Plot correct RT's.
+        """Plot correct RT's."""
 
-        Parameters
-        ----------
-        show
-        ylim
-        xlabel
-        ylabel
-        cond_labels
-        kwargs
-        """
+        kwargs.setdefault("ylabel", "RT Correct [ms]")
 
         _plot_beh(
             self.dat.summary["rt_cor"],
             cond_labels,
-            ylim,
-            xlabel,
-            ylabel,
             False,
             **kwargs,
         )
@@ -477,35 +375,14 @@ class DmcPlot:
         if show:
             plt.show(block=False)
 
-    def plot_er(
-        self,
-        show=True,
-        ylim=None,
-        xlabel=None,
-        ylabel="Error Rate [%]",
-        cond_labels=("Compatible", "Incompatible"),
-        **kwargs
-    ):
-        """Plot error rate
+    def plot_er(self, show=True, cond_labels=("Compatible", "Incompatible"), **kwargs):
+        """Plot error rate"""
 
-        Parameters
-        ----------
-        show
-        ylim
-        xlabel
-        ylabel
-        label_fontsize
-        tick_fontsize
-        cond_labels
-        kwargs
-        """
+        kwargs.setdefault("ylabel", "Error Rate [%]")
 
         _plot_beh(
             self.dat.summary["per_err"],
             cond_labels,
-            ylim,
-            xlabel,
-            ylabel,
             True,
             **kwargs,
         )
@@ -514,32 +391,15 @@ class DmcPlot:
             plt.show(block=False)
 
     def plot_rt_error(
-        self,
-        show=True,
-        ylim=None,
-        xlabel=None,
-        ylabel="RT Error [ms]",
-        cond_labels=("Compatible", "Incompatible"),
-        **kwargs
+        self, show=True, cond_labels=("Compatible", "Incompatible"), **kwargs
     ):
-        """Plot error RT's.
+        """Plot error RT's."""
 
-        Parameters
-        ----------
-        show
-        ylim
-        xlabel
-        ylabel
-        cond_labels
-        kwargs
-        """
+        kwargs.setdefault("ylabel", "RT Error [ms]")
 
         _plot_beh(
             self.dat.summary["rt_err"],
             cond_labels,
-            ylim,
-            xlabel,
-            ylabel,
             False,
             **kwargs,
         )
@@ -548,7 +408,7 @@ class DmcPlot:
             plt.show(block=False)
 
 
-def _plot_beh(dat, cond_labels, ylim, xlabel, ylabel, zeroed, **kwargs):
+def _plot_beh(dat, cond_labels, zeroed, **kwargs):
     """Internal function to plot rt/er for comp vs. comp"""
 
     kwargs.setdefault("color", "black")
@@ -560,11 +420,11 @@ def _plot_beh(dat, cond_labels, ylim, xlabel, ylabel, zeroed, **kwargs):
     plt.margins(x=0.5)
 
     if zeroed:
-        ylim = ylim or [0, np.max(dat) + 5]
+        kwargs.setdefault("ylim", [0, np.max(dat) + 5])
     else:
-        ylim = ylim or [np.min(dat) - 100, np.max(dat) + 100]
+        kwargs.setdefault("ylim", [np.min(dat) - 100, np.max(dat) + 100])
 
-    _adjust_plt(ylim=ylim, xlabel=xlabel, ylabel=ylabel, **kwargs)
+    _adjust_plt(**kwargs)
 
 
 def _filter_dict(given, allowed):
